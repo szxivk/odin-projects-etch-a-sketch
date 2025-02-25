e7a18ee60ab2f9762a0eec95d3fc32766c16ad68
szxivk <50396748+szxivk@users.noreply.github.com>
2025-02-25 14:21:50 +0000

    Add square grid as sketch-screen
    - used flexbox and DOM manipulation
    - m*n grid where m=n
    - added basic styles for now

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-console.log("Goodbye, World!");
-// lmao
+function makeGrid(size) {
+  let screen = document.querySelector(".sketch-screen");
+  for (let i = 0; i < size; i++) {
+    let column = document.createElement("div");
+    column.classList.add("column");
+    for (let j = 1; j <= size; j++) {
+      let row = document.createElement("div");
+      row.classList.add("row");
+      row.style.border = "1px solid black";
+
+      column.appendChild(row);
+    }
+    screen.appendChild(column);
+  }
+}
+
+makeGrid(64);
