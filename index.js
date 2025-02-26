fa681000235f78df33021065f3c594036a48bae8
szxivk <50396748+szxivk@users.noreply.github.com>
2025-02-26 07:47:14 +0000

    Add “hover” effect to grid cells
    - changes background color of cells when mouse is hovered over them
    - used event listeners
    - also added pixelated trail effect

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 //need to set a limit for size to be max 100 or 64 later
 
+const screen = document.querySelector(".sketch-screen");
+screen.addEventListener("mouseover", (e) => {
+  if (e.target.className === "cell") {
+    e.target.style.backgroundColor = "#000000";
+
+    setTimeout(() => {
+      e.target.style.backgroundColor = "#dcdcd8";
+    }, 500);
+  }
+});
+
+makeGrid(16);
+
 function makeGrid(size) {
-  let screen = document.querySelector(".sketch-screen");
   for (let i = 0; i < size; i++) {
     let row = document.createElement("div");
     row.classList.add("row");
     for (let j = 1; j <= size; j++) {
       let cell = document.createElement("div");
       cell.classList.add("cell");
-
+      // cell.style.transition = "background-color 0.3s ease-out";
       row.appendChild(cell);
     }
     screen.appendChild(row);
   }
 }
-
-makeGrid(16);
 
 //add hovering effect
