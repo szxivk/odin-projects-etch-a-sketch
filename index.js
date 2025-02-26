deffb710f8c2a8393d2a109e4169ea64815b949f
szxivk <50396748+szxivk@users.noreply.github.com>
2025-02-26 06:19:16 +0000

    Update sketch-screen grid
    - replaced previous variable names with appropriate var names for better readability

--- index.js
+++ index.js
@@ -3,15 +3,15 @@
 function makeGrid(size) {
   let screen = document.querySelector(".sketch-screen");
   for (let i = 0; i < size; i++) {
-    let column = document.createElement("div");
-    column.classList.add("row");
+    let row = document.createElement("div");
+    row.classList.add("row");
     for (let j = 1; j <= size; j++) {
-      let row = document.createElement("div");
-      row.classList.add("cell");
+      let cell = document.createElement("div");
+      cell.classList.add("cell");
 
-      column.appendChild(row);
+      row.appendChild(cell);
     }
-    screen.appendChild(column);
+    screen.appendChild(row);
   }
 }
 
